b861a4cda8133c5906f291d2312c9865a165061a
VATuan2710 <vatbgct@gmail.com>
2024-11-26 18:16:28 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/header/Header";
 import ProductList from "./components/productList/ProductList";
 import Footer from "./components/footer/Footer";
+import Shop from "./pages/Shop";
 import "./App.css";
 
 function App() {
@@ -14,8 +15,9 @@ function App() {
   return (
     <div className={darkMode ? "dark-mode" : "light-mode"}>
       <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
-      <ProductList />
-      <Footer />
+      {/* <ProductList /> */}
+      <Shop />
+      {/* <Footer /> */}
     </div>
   );
 }
